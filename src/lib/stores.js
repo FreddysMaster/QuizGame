905a4db88729b0442679be0058e6a2b7cc0f0bd3
FreddysMaster <agervai1@student.fitchburgstate.edu>
2024-02-15 19:28:24 -0500

    Make the ui more responsive

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -1,3 +1,18 @@
 import { writable } from 'svelte/store';
 
 export const score = writable(0);
+
+export const categories = [
+  "history",
+  "film_and_tv",
+  "society_and_culture",
+  "geography",
+  "arts_and_literature",
+  "science",
+  "music",
+  "sport_and_leisure",
+  "food_and_drink",
+  "general_knowledge",
+];
+
+export const selectedCategories = writable(categories);
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,38 +1,28 @@
 <script>
-	import "$lib/styles.css";
-  import { InlineModal } from 'svelte-inline-modal'
+  import "$lib/styles.css";
 </script>
 
 <main>
-<nav>
-  <InlineModal let:open>
-    <!-- Open Button -->
-    <button class="login-button" on:click={open}>Login</button>
-  
-    <!-- Modal Content -->
-    <div slot="menu" let:close>
-      <h2>It's Modal Menu</h2>
-  </InlineModal>
-
-</nav>
-
-<slot />
+  <nav>
+      <button class="login-button">Login</button>
+  </nav>
+  <slot />
 </main>
 
 <style>
-nav {
-  display: flex;
-  justify-content: flex-end;
-}
+  nav {
+    display: flex;
+    justify-content: flex-end;
+  }
 
-.login-button {
-  border:  0.25em solid var(--border-color);
-  background: var(--primary-color);
-  border-radius:  10px;
-  font-size: larger;
-  padding:  .5em;
-  cursor: pointer;
-  width:  5em;
-  color: var(--button-text-color);
-}
+  .login-button {
+    border: 0.25em solid var(--border-color);
+    background: var(--primary-color);
+    border-radius: 10px;
+    font-size: larger;
+    padding: 0.5em;
+    cursor: pointer;
+    width: 5em;
+    color: var(--button-text-color);
+  }
 </style>
