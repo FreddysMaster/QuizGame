15beeb1be394c12303d531fb31512e369b734048
FreddysMaster <agervai1@student.fitchburgstate.edu>
2024-03-07 13:44:10 -0500

    WIP login

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,12 +1,19 @@
 export const load = async ({ locals }) => {
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+
+>>>>>>> 14455ff (WIP login)
   return {
     user: locals.user,
   }
 }
+<<<<<<< HEAD
 =======
     return {
       user: locals.user,
     }
   }
 >>>>>>> ce841bb (moved to tailwind)
+=======
+>>>>>>> 14455ff (WIP login)
