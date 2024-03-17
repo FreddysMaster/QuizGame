ce841bb5e885d43e9d0bcd3e03d0e861ce1f1460
FreddysMaster <agervai1@student.fitchburgstate.edu>
2024-03-17 15:43:59 -0400

    moved to tailwind

---
+++ src/routes/profile/+page.svelte
@@ -0,0 +1,71 @@
+<script>
+    import "$lib/app.css";
+    import { superForm } from 'sveltekit-superforms';
+    export let data;
+
+    const { form, errors, enhance } = superForm(data.form);
+</script>
+
+<main>
+    <h1 class="text-3xl font-bold">My Profile</h1>
+    <form method="POST" use:enhance>
+    <div class="form-control w-full max-w-sm">
+        <label for="username" class="label">
+            <span class="label-text font-bold">Username</span>
+        </label>
+        <input
+            type="text"
+            name="username"
+            class="input input-primary w-screen max-w-sm {$form.username
+                ? 'input-error'
+                : 'input-bordered'}"
+            placeholder="Enter your username"
+            bind:value={$form.username}
+        />
+        <label for="username" class="label">
+            {#if $errors.username}
+                <span class="label-text-alt text-error">{$errors.username}</span>
+            {/if}
+        </label>
+    </div>
+    <div class="form-control w-full max-w-sm">
+        <label for="email" class="label">
+            <span class="label-text font-bold">Email</span>
+        </label>
+        <input
+            type="text"
+            name="email"
+            class="input input-primary w-full max-w-sm {$form.email
+                ? 'input-error'
+                : 'input-bordered'}"
+            placeholder="Enter your email"
+            bind:value={$form.email}
+        />
+        <label for="email" class="label">
+            {#if $errors.email}
+                <span class="label-text-alt text-error">{$errors.email}</span>
+            {/if}
+        </label>
+    </div>
+    <h1 class="text-3xl font-bold">Reset Password</h1>
+    <div class="form-control w-full max-w-sm">
+        <label for="password" class="label">
+            <span class="label-text font-bold">Reset Password</span>
+        </label>
+        <input
+            type="password"
+            name="password"
+            class="input input-primary w-full max-w-md {$form.password
+                ? 'input-error'
+                : 'input-bordered'}"
+            placeholder="Enter your password"
+            bind:value={$form.password}
+        />
+        <label for="password" class="label">
+            {#if $errors.password}
+                <span class="label-text-alt text-error">{$errors.password}</span>
+            {/if}
+        </label>
+    </div>
+</form>
+</main>
---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#3b82f6",
+          "secondary": "#d077e5",
+          "accent": "#e8efff",
+          "neutral": "#140f00",
+          "base-100": "#fff",
+          "info": "#00e1ff",
+          "success": "#4ade80",
+          "warning": "#f59e0b",
+          "error": "#ef4444",
+        },
+      },
+    ],
+  },
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+}
