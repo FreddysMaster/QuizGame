1e8ffa62516f4171280233e84b7ce554806edb93
FreddysMaster <agervai1@student.fitchburgstate.edu>
2024-03-18 11:38:16 -0400

    fix

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,22 +1,5 @@
 export const load = async ({ locals }) => {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
->>>>>>> 14455ff (WIP login)
-=======
->>>>>>> 0ca72a5 (Added a dynamic nav bar based on if the user is logged in as well as the user type)
   return {
     user: locals.user,
   }
 }
-<<<<<<< HEAD
-=======
-    return {
-      user: locals.user,
-    }
-  }
->>>>>>> ce841bb (moved to tailwind)
-=======
->>>>>>> 14455ff (WIP login)
